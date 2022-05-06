467ae04f08ca3b48887c2965c7e2cb97f221437e
沈国家 <shenguojia@shenguojiadeMacBook-Pro.local>
2022-05-06 16:44:34 +0800

    test(utils): 测试

--- src/views/home.vue
+++ src/views/home.vue
@@ -13,6 +13,6 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'H-ome'
+  name: 'Home'
 })
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import importElementPlus from 'vite-plugin-element-plus'
+// import importElementPlus from 'vite-plugin-element-plus'
+// eslint-disable-next-line import/no-unresolved
+import AutoImport from 'unplugin-auto-import/vite'
+// eslint-disable-next-line import/no-unresolved
+import Components from 'unplugin-vue-components/vite'
+// eslint-disable-next-line import/no-unresolved
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +24,72 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue(), importElementPlus({ useSource: true })],
+  plugins: [
+    vue(),
+    // importElementPlus({ useSource: true }),
+
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        // custom
+        {
+          '@vueuse/core': [
+            // named imports
+            'useMouse', // import { useMouse } from '@vueuse/core',
+            // alias
+            ['useFetch', 'useMyFetch'] // import { useFetch as useMyFetch } from '@vueuse/core',
+          ],
+          axios: [
+            // default imports
+            ['default', 'axios'] // import { default as axios } from 'axios',
+          ],
+          '[package-name]': [
+            '[import-names]',
+            // alias
+            ['[from]', '[alias]']
+          ]
+        }
+      ],
+
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+      resolvers: [ElementPlusResolver()],
+      // dts: 'src/auto-imports.d.ts'
+      dts: false
+    }),
+    Components({
+      // valid file extensions for components.
+      dts: 'src/component.d.ts',
+      // dts: false,
+      // filters for transforming targets
+      exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass'
+        })
+      ]
+    })
+  ],
   base: './', // 设置打包路径
   server: {
     port: 4000, // 设置服务启动端口号
