c69eb08adec0369e6906fa2c9ce1c27f3ea3184f
Kaier-shen <Kaier-shen@users.noreply.github.com>
2022-05-07 01:57:57 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter af995b97c694189b8e4210eed9b07a4b0d178831

---
+++ assets/axios.c1833c2e.js
@@ -0,0 +1,1 @@
+import{_ as e,d as o,c as n,o as s}from"./index.6709c73d.js";const a=o({name:"Axios"});function t(r,c,i,p,_,d){return s(),n("div",null,"Axios")}var l=e(a,[["render",t]]);export{l as default};
